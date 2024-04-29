28fd071dfb19e1c270b6ce78ce343b6ab67ab03e
jorgesalesianos <leon.pejor23@zaragoza.salesianos.edu>
2024-04-29 20:04:51 +0200

    Jorge Leon 20:03

--- ClaseRosana.java
+++ ClaseRosana.java
@@ -3,12 +3,10 @@ public class ClaseRosana
 
     String nombre;
 
-    public ClaseRosana(String as_nombre)
+    public ClaseLeon(String as_nombre)
     {
         nombre = as_nombre;   
     }
-
-    // clase donde se escribe el nombre
     public void escribirnombre()
     {
         System.out.println("Mi nombre es: " + nombre);
