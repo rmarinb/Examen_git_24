feb6eb04049e169761a546f8d792cabc0166ecfd
rmarinb <93933016+rmarinb@users.noreply.github.com>
2024-04-29 14:57:31 +0200

    Add files via upload

--- Examen.java
+++ Examen.java
@@ -3,8 +3,87 @@ public class Examen
     public static void main(String[] args)
     {
         // Ejemplo profesora:
-        ClaseRosana Rosana = new ClaseRosana("Ros");
+        ClaseRosana Rosana = new ClaseRosana("Rosana Marín");
         Rosana.escribirnombre();
-       
+/*
+        ClaseAlomoto Alo = new ClaseAlomoto("David Alomoto");
+        Alo.escribirnombre();
+
+        ClaseArantegui Alejandro = new ClaseArantegui("Alejandro Arantegui");
+        Alejandro.escribirnombre();
+
+        ClaseVir Vir = new ClaseVir("Virginia Arias");
+        Vir.escribirnombre();
+
+        ClaseAlex Chicaiza = new ClaseAlex("Alex Chicaiza");
+        Chicaixa.escribirnombre();
+
+        ClaseManuel Manuel = new ClaseManuel("Manuel Claveria");
+        Manuel.escribirnombre();
+
+        ClaseJorge Jorge = new ClaseJorge("Jorge Contin");
+        Jorge.escribirnombre();
+
+        ClaseHecto Hector = new ClaseHector("Hector García");
+        Hector.escribirnombre();
+
+        ClaseLeon Leon = new ClaseLeon("Jorge Leon");
+        Leon.escribirnombre();
+
+        ClaseLerida Lerida = new ClaseLerida("Daniel Lerida");
+        Lerida.escribirnombre();
+
+        ClaseLucas Lucas = new ClaseLucas("Lucas Lopez");
+        Lucas.escribirnombre();
+
+        ClaseCesar Cesar = new ClaseCesar("Cesar Marco");
+        Cesar.escribirnombre();
+
+        ClaseAna Ana = new ClaseAna("Ana Martínez");
+        Ana.escribirnombre();
+
+        ClaseBerman Berman = new ClaseBerman("Berman Martínez");
+        Berman.escribirnombre();
+
+        ClaseSilvia Silvia = new ClaseSilvia("Silvia Monterde");
+        Silvia.escribirnombre();
+
+        ClaseSaul Saula = new ClaseSaul("Saul Morte");
+        Saul.escribirnombre();
+
+        ClaseAlvaro Alvaro = new ClaseAlvaro("Alvaro Navarro");
+        GianAlvaro.escribirnombre();
+
+        Clasecarlos Carlos = new Clasecarlos("Carlos Perez");
+        Carlos.escribirnombre();
+
+        ClaseFloren Floren  = new ClaseFloren("Florentina Pedra");
+        Floren.escribirnombre();
+
+        ClaseLuigi Luigi = new ClaseLuigi("Luigi");
+        Luigi.escribirnombre();
+
+        ClaseSheyla Sheyla = new ClaseSheyla("Sheyla Resmella");
+        Sheyla.escribirnombre();
+
+        ClaseRuben Ruben = new ClaseRuben("Ruben Reula");
+        Ruben.escribirnombre();
+
+        ClaseSanchez Sanchez= new ClaseSanchez("Alejandro Sanchez");
+        Sanchez.escribirnombre();
+
+        ClaseBruce Bruce= new ClaseBruce("Bruce Simbaña");
+        Bruce.escribirnombre();
+
+        ClaseTrif Trif= new ClaseTrif("Adrian Trif");
+        Trif.escribirnombre();
+
+        ClaseVal Vale= new ClaseVal("Marcos Val");
+        Val.escribirnombre();   
+
+
+        */
+
+        
     }
 }
