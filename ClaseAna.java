22279c3785eeda39d2fc7e6094fa04d462af384d
anamprc <anamartinezaparicio@gmail.com>
2024-04-29 19:53:58 +0200

    Ana Martinez 19:53

---
+++ ClaseAna.java
@@ -0,0 +1,18 @@
+package examen;
+
+public class ClaseAna {
+	
+    String nombre;
+
+    public ClaseAna(String as_nombre)
+    {
+        nombre = as_nombre;   
+    }
+
+    // clase donde se escribe el nombre
+    public void escribirnombre()
+    {
+        System.out.println("Mi nombre es: " + nombre);
+    }
+
+}
